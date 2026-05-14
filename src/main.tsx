a284fc1c80bf4703276853e8b1a251233fd983d9
buiviet666 <wandering.isme@gmail.com>
2026-05-14 16:54:41 +0700

    [feat] Draft: update library CSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'antd/dist/reset.css';
+import 'virtual:uno.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 
